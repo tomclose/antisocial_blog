d7d5532c02b3924171a5a7339daaea8cf07c044c
Tom Close <tom.close@cantab.net>
2013-08-28 12:37:42 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AntisocialBlog::Application.config.secret_key_base = 'e419c92f1e4ab5e9d847ba58d36a2510f03c7466fb3dab177fa7f3b1a86d77a30da72fe81ee592cbbb3b1e587c288eff675233402a6c8ba0c4d373c7e3a6fe65'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AntisocialBlog::Application.config.session_store :cookie_store, key: '_antisocial_blog_session'
