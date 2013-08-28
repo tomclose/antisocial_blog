16098b07cb92ed12c81258d80c1e755151036489
Tom Close <tom.close@cantab.net>
2013-08-28 13:35:14 +0100

    Post verifies presence of title. Additional integration test to check this.

--- test/integration/create_post_test.rb
+++ test/integration/create_post_test.rb
@@ -11,4 +11,15 @@ class CreatePostTest < ActionDispatch::IntegrationTest
     assert page.has_content?('Post was successfully created.')
     assert page.has_content?('My first post')
   end
+
+  test 'rejects post with no title' do
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Title', with: ''
+    fill_in 'Date', with: '28/8/2013'
+    fill_in 'Body', with: "What a splendid post this is."
+    click_button 'Create Post'
+    assert page.has_content?("Title can't be blank")
+    assert page.has_content?('What a splendid post this is.') # check we have old info
+  end
 end
